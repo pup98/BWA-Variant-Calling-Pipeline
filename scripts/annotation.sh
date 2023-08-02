6829c8d2c0269fd6f3cdc42d1065da06d6bc3fc6
Ujjwal <99062865+pup98@users.noreply.github.com>
2023-08-02 15:18:05 +0530

    Update annotation.sh

--- scripts/annotation.sh
+++ scripts/annotation.sh
@@ -4,11 +4,11 @@ output="/home/test/users/ujjwal/new_project/annotated_files"
 db="/home/ngs/Tools/humandb/"
 input="/home/test/users/ujjwal/new_project/variant_files"
 
-perl /home/ngs/Tools/annovar/convert2annovar.pl ${input}/sample1.snp.vcf -format vcf4 > ${output}/sample1_anno.vcf4
-perl /home/ngs/Tools/annovar/convert2annovar.pl ${input}/sample2.snp.vcf -format vcf4 > ${output}/sample2_anno.vcf4
+perl /home/ngs/Tools/annovar/convert2annovar.pl -format vcf4 ${input}/sample1.snp.vcf > ${output}/sample1.avinput
+perl /home/ngs/Tools/annovar/convert2annovar.pl -format vcf4 ${input}/sample2.snp.vcf > ${output}/sample2.avinput
 
-perl /home/ngs/Tools/annovar/annotate_variation.pl -geneanno ${output}/sample1_anno.vcf4 -buildver hg38 ${db} 
-perl /home/ngs/Tools/annovar/annotate_variation.pl -geneanno ${output}/sample2_anno.vcf4 -buildver hg38 ${db} 
+perl /home/ngs/Tools/annovar/annotate_variation.pl -geneanno -buildver hg38 ${output}/sample1.avinput ${db} 
+perl /home/ngs/Tools/annovar/annotate_variation.pl -geneanno -buildver hg38 ${output}/sample2.avinput ${db} 
 
 
 echo "Annotation complete"
